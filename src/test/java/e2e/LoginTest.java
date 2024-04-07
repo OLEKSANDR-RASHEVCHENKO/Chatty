0bdfe53240e9374acc4cd3f38a5b3e82299bc094
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-04-07 08:58:36 +0200

    Added LoginTest.javaTest

--- src/test/java/e2e/LoginTest.java
+++ src/test/java/e2e/LoginTest.java
@@ -8,6 +8,20 @@ public class LoginTest extends BaseTest{
     LoginPage loginPage;
     HomeblogPage homeblogPage;
 
+    private void loginTestMethod(String email, String password, String screenshotName, boolean negativeCase) {
+        loginPage = new LoginPage(app.driver);
+        loginPage.waitForLoading();
+        loginPage.login(email, password);
+        if (negativeCase) {
+            loginPage.waitForLoading();
+            loginPage.takeLoginPageScreenshot(screenshotName);
+        } else {
+            homeblogPage = new HomeblogPage(app.driver);
+            homeblogPage.waitForLoading();
+            homeblogPage.takeScreenshotHeader();
+        }
+    }
+
     @Test(description = "User can login with valid data as a user")
     public void userCanLogin(){
         String email = "tatyanaskv@rambler.ru";
@@ -31,4 +45,39 @@ public class LoginTest extends BaseTest{
         homeblogPage = new HomeblogPage(app.driver);
         homeblogPage.waitForLoading();
     }
+
+
+
+
+    @Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
+    public void userCannotLogin(String email, String password, String caseName) {
+        loginPage = new LoginPage(app.driver);
+        loginPage.waitForLoading();
+        loginPage.login(email, password);
+
+        loginPage.waitForLoading();
+        loginPage.takeLoginPageScreenshot(caseName + "_negative_login_case");
+    }
+
+
+    @Test
+    public void userCanLoginWithValidData() {
+        loginTestMethod("newtest@gmail.com", "newtest@gmail.com", null, false);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidEmail() {
+        loginTestMethod("newtestgmail.com", "newtest@gmail.com", "login_invalid_email", true);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidPassword() {
+        loginTestMethod("newtest@gmail.com", "newtestgmail.com", "login_invalid_password", true);
+    }
+
+    @Test
+    public void userCanLoginWithInvalidEmailAndPassword() {
+        loginTestMethod("newtestgmail.com", "newtestgmail.com", "login_invalid_email_and_password", true);
+    }
+}
 }
