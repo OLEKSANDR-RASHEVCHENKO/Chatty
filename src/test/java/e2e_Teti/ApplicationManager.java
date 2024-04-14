92026e9705dff63468440e9a8c3b81a9e1e1daad
Tetiana Skvarnyk <t.skvarnikgmail.com>
2024-04-14 19:57:57 +0200

    Added ApiTests

--- src/test/java/e2e_Teti/ApplicationManager.java
+++ src/test/java/e2e_Teti/ApplicationManager.java
@@ -1,13 +1,18 @@
-package e2e_Teti;
+package e2e;
 
 import config.Config;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.ByteArrayInputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.HashMap;
@@ -17,7 +22,7 @@ public class ApplicationManager {
     private final Config config = new Config();
     public WebDriver driver;
 
-    protected void init() {
+    public void init() {
         if (config.getSelenoidState()) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName("chrome");
@@ -34,18 +39,25 @@ public class ApplicationManager {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
-
-
         } else {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().clearResolutionCache().setup();
+            ChromeOptions options = new ChromeOptions();
+            if (config.getHeadless()) {
+                options.addArguments("--headless");
+            }
+            options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
         }
         driver.get(config.getProjectUrl());
         driver.manage().window().setSize(new Dimension(config.getWindowWeight(), config.getWindowHeight()));
 
     }
 
-    protected void stop() {
+    public void stop(boolean testPassed) {
+        if (!testPassed) {
+            byte[] screenshotData = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment("Screenshot on failure", new ByteArrayInputStream(screenshotData));
+        }
         driver.quit();
     }
 }
--- src/test/java/e2e_Teti/BaseTest.java
+++ src/test/java/e2e_Teti/BaseTest.java
@@ -4,12 +4,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static e2e.ApplicationManager app = new e2e.ApplicationManager();
 
     @BeforeMethod
     public void setupTest() {
         app.init();
     }
+
     @AfterMethod
     public void tearDown() {
         app.stop();
