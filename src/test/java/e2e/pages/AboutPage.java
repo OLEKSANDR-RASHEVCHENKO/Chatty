76edd77673d324dc7d7720b63d09b1687a1d7f7d
AliaksandrPakhamovich <alexberr9501@gmail.com>
2024-04-27 16:28:08 +0200

    Added Feedback test

--- src/test/java/e2e/pages/AboutPage.java
+++ src/test/java/e2e/pages/AboutPage.java
@@ -3,19 +3,21 @@ package e2e.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
-public class AboutPage extends BasePage{
+public class AboutPage extends BasePage {
     public AboutPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath ="//*[@href='/about']")
+    @FindBy(xpath = "//*[@href='/about']")
     WebElement aboutButton;
     @FindBy(xpath = "//*[@class='about-box']")
     WebElement aboutBox;
 
-    public void waitForLoading(){
+    public void waitForLoading() {
         getWait().forVisibility(aboutBox);
+        Assert.assertTrue(aboutBox.isDisplayed());
     }
 
 }
